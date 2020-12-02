4660f571ff4fd8a1ce243d549aabebbc79448bff
daria-dudka <d.svirskaya@gmail.com>
2020-12-02 22:58:34 +0200

    changed the scroll-btn js

--- src/js/main.js
+++ src/js/main.js
@@ -14,34 +14,20 @@ $(function() {
   document.getElementById('checkbox_phone').onchange = toggle
   ;
 
-  let position = $(window).scrollTop() + "px";
 
-  $(window).on('scroll', function() {
-    let scroll = $(window).scrollTop() + "px";
-    const scrollBtn = document.getElementById('scroll-btn');
-    const width = $(window).width();
+  
+  let position = window.pageYOffset;
 
-    if (scroll < position && width < 760) {
-      scrollBtn.style.display = 'block';
-    } else {
-      scrollBtn.style.display = 'none';
-    }
-    position = scroll;
+  window.addEventListener('scroll', () => { 
+
+  let scroll = window.pageYOffset; 
+  const scrollBtn = document.getElementById('scroll-btn');
+  
+  scroll < position && window.matchMedia("(max-width: 760px)").matches ? scrollBtn.style.display = 'block' : scrollBtn.style.display = 'none';
+
+  position = scroll;
+
   });
-
-  
-//   let position = window.pageYOffset;
-
-//   window.addEventListener('scroll', () => { 
-
-//   let scroll = window.pageYOffset; 
-//   const scrollBtn = document.getElementById('scroll-btn');
-  
-//   scroll < position ? scrollBtn.style.display = 'block' : scrollBtn.style.display = 'none';
-
-//   position = scroll;
-
-// });
 
 
 });
