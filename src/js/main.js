a7917f445c1bddfbc30236780780c2dfaa740ffa
daria-dudka <d.svirskaya@gmail.com>
2020-12-02 13:17:07 +0200

    added scroll-btn and fixed mobile bugs

--- src/js/main.js
+++ src/js/main.js
@@ -2,24 +2,30 @@ $(function() {
 
   $('input').styler();
 
+
+
   function toggle() {
     const formPhone = document.getElementById('form__phone');
     if(this.checked)
     formPhone.style.display = 'block';
     else
     formPhone.style.display = 'none'
-      }
-  document.getElementById('checkbox_phone').onchange = toggle;
+  };
+  document.getElementById('checkbox_phone').onchange = toggle
+  ;
 
-  $(window).scroll(function (event) {
-    const reservationBtn = document.getElementById('reservation-btn');
-    const top = $(window).scrollTop();
-     if(top >2000){
-      reservationBtn.style.display = 'block';
-     } else {
-      reservationBtn.style.display = 'none'
-     }
-  });
+
+  
+  $(window).on('mousewheel', function(event) {
+    const scrollBtn = document.getElementById('scroll-btn');
+    const width = $(window).width();
+    if (event.originalEvent.wheelDelta >= 0 && width <= 760 ) {
+      scrollBtn.style.display = 'block';
+    }
+    else {
+      scrollBtn.style.display = 'none';
+    }
+    });
 
 });
 
