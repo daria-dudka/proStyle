62153d5705411fc5edf16fa4b2238a1d3aeae9d2
daria-dudka <d.svirskaya@gmail.com>
2020-12-02 15:05:37 +0200

    fixed bugs

--- src/js/main.js
+++ src/js/main.js
@@ -14,20 +14,34 @@ $(function() {
   document.getElementById('checkbox_phone').onchange = toggle
   ;
 
-  var position = $(window).scrollTop();
+  let position = $(window).scrollTop() + "px";
 
-  $(window).on('scroll', () => {
-      let scroll = $(window).scrollTop();
-      const scrollBtn = document.getElementById('scroll-btn');
-      const width = $(window).width();
+  $(window).on('scroll', function() {
+    let scroll = $(window).scrollTop() + "px";
+    const scrollBtn = document.getElementById('scroll-btn');
+    const width = $(window).width();
 
-      if (scroll < position && width < 760) {
-        scrollBtn.style.display = 'block';
-      } else {
-        scrollBtn.style.display = 'none';
-      }
-      position = scroll;
-    });
+    if (scroll < position && width < 760) {
+      scrollBtn.style.display = 'block';
+    } else {
+      scrollBtn.style.display = 'none';
+    }
+    position = scroll;
+  });
+
+  
+//   let position = window.pageYOffset;
+
+//   window.addEventListener('scroll', () => { 
+
+//   let scroll = window.pageYOffset; 
+//   const scrollBtn = document.getElementById('scroll-btn');
+  
+//   scroll < position ? scrollBtn.style.display = 'block' : scrollBtn.style.display = 'none';
+
+//   position = scroll;
+
+// });
 
 
 });
